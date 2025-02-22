4fe3d8779f1fc926eb87e68b819a2bf9062cfd9f
Nishanth K S <nishanth.kkps@gmail.com>
2025-02-22 20:51:11 +0530

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,7 @@ export default async function Home() {
 
   return (
     <div>
+      {"hi"}
       {users.length > 0 ? (
         users.map((user) => <div key={user.id}>{user.username}</div>)
       ) : (
